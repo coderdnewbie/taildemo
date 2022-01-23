0b300ca4832cb253cca41df167005a4f9c738190
coderdnewbie <ajaytandonphd@gmail.com>
2022-01-23 09:18:43 +0000

    taildemo change buton text to Hello There

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+     <div>
+       <button type="button" className="mt-3 ml-3 px-4 py-3 font-semibold border rounded-full border-gray-800 text-gray-800 hover:text-red-500 hover:bg-yellow-300">
+         Hello There     
+       </button>
+     </div>
+    </>
+  )
+}
+
+export default App;
