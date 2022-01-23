dad80b6ce9d7dbd373067277f2956cd678daa13a
coderdnewbie <ajaytandonphd@gmail.com>
2022-01-23 21:15:43 +0000

    added tabs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from 'react';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
-    <>
-     <div>
-       <button type="button" className="mt-3 ml-3 px-4 py-3 font-semibold border rounded-full border-gray-800 text-gray-800 hover:text-red-500 hover:bg-yellow-300">
-         Hello There     
-       </button>
-     </div>
-    </>
-  )
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function about() {
+  return (
+    <section id="about"> 
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+          <div className="text-4xl text-transparent bg-clip-text bg-gradient-to-bl from-purple-300 to-blue-900">          
+              Hi, This is a  
+            </div>            
+            <div className="text-4xl text-yellow-400 mt-4 text-center">
+               Demo Site
+            </div>
+          </h1>
+          <p className="mt-12 mb-8 leading-relaxed">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <div className="flex justify-center">
+            <a
+            href="#contact"
+            className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+              Contact
+            </a>
+            <a
+            href="#projects"
+            className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              Projects
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          Below are Tabs
+        </div>
+      </div>    
+    </section>
+    );
+}
+
+export default about;
