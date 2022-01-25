e2817cfc403504151503f77241dfbcbb5449dff5
coderdnewbie <ajaytandonphd@gmail.com>
2022-01-25 17:48:04 +0000

    Fix dark mode issues

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -4,7 +4,7 @@ function Feature() {
     return (	
         <section className="text-gray-400 bg-gray-900 ">
             <div className="container flex flex-col-reverse mx-auto lg:flex-row">
-                <div className="flex flex-col px-6 py-8 space-y-6 rounded-sm sm:p-8 lg:p-12 lg:w-1/2 xl:w-2/5 dark:bg-violet-400 dark:text-coolGray-900">
+                <div className="flex flex-col px-6 py-8 space-y-6 rounded-sm sm:p-8 lg:p-12 lg:w-1/2 xl:w-2/5 ">
                     <div className="flex space-x-2 sm:space-x-4">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="flex-shrink-0 w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -8,7 +8,7 @@ function SignUp() {
             <h1 className="text-5xl font-bold leading-none text-center">Sign up now</h1>
             <p className="text-xl font-medium text-center">At a assumenda quas cum earum ut itaque commodi saepe rem aspernatur quam natus quis nihil quod, hic explicabo doloribus magnam neque, exercitationem eius sunt!</p>
             <div className="flex flex-col space-y-4 sm:space-y-0 sm:flex-row sm:space-x-8">
-                <button className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-400 dark:text-coolGray-900">Get started</button>
+                <button className="px-8 py-3 text-lg font-semibold rounded ">Get started</button>
                 <button className="px-8 py-3 text-lg font-normal border rounded dark:bg-coolGray-100 dark:text-coolGray-900 dark:border-coolGray-300">Learn more</button>
             </div>
         </div>
