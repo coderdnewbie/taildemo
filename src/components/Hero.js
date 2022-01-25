fd439827714645b8feac6197cec575f2b4d93102
coderdnewbie <ajaytandonphd@gmail.com>
2022-01-25 16:56:50 +0000

    Fixed dark mode issue

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Hero() {
     return (
         <section>
-            <div className="dark:bg-violet-400">
+            <div className="bg-gray-700">
                 <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 dark:text-coolGray-900">
                     <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl dark:text-coolGray-900">Provident blanditiis cum exercitationem</h1>
                     <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-coolGray-900">Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab amet vero eaque explicabo!</p>
